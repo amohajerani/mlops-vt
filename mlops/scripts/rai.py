38b75f78c550ac84a18191367c26dd31f535b2fa
Amirhossein Mohajerani <amohajerani@dynamic-oit-visitornet101-10-25-29-146.princeton.org>
2024-04-04 15:38:53 -0400

    none

--- mlops/scripts/rai.py
+++ mlops/scripts/rai.py
@@ -99,7 +99,7 @@ def rai_regression_pipeline(
         title="RAI Dashboard for Visit Time Prediction",
         task_type="regression",
         model_info=model_id,
-        model_input=Input(type="mlflow_model", path="azureml:vt-model:49"),
+        model_input=Input(type="mlflow_model", path=model_path),
         train_dataset=train_data,
         test_dataset=test_data,
         target_column_name=target_column_name,
