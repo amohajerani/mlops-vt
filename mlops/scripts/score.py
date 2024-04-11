4f827ba42c4e1434aac2294265f5fb6fbe500eb4
Amirhossein Mohajerani <amohajerani@SH-MV0X9HMQP6.local>
2024-04-11 09:39:19 -0400

    bugfix

--- mlops/scripts/score.py
+++ mlops/scripts/score.py
@@ -2,15 +2,17 @@ import os
 import json
 import numpy
 import joblib
-import pyodbc
 import pandas as pd
 from inference_schema.schema_decorators import input_schema, output_schema
 from inference_schema.parameter_types.pandas_parameter_type import PandasParameterType
 from inference_schema.parameter_types.standard_py_parameter_type import (
     StandardPythonParameterType,
 )
+import logging
 
-connection_string='Driver={ODBC Driver 17 for SQL Server};Server=tcp:vt-ml-srvr.database.windows.net,1433;Database=vt-ml-db;Uid=vt-sql-admin-login;Pwd=College1//;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=60;'  
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("score.py")
+
 
 def init():
     """
@@ -21,130 +23,83 @@ def init():
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
     # Please provide your model's folder name if there is one
-    model_path = os.path.join(
-        os.getenv("AZUREML_MODEL_DIR"), "vt-model/model.pkl"
-    )
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "vt-model/model.pkl")
     # deserialize the model file back into a sklearn model
     model = joblib.load(model_path)
+
 
 @input_schema(
     param_name="input_data",
     param_type=PandasParameterType(
         pd.DataFrame(
             {
-                "PROVIDERIDS": [
-                  "000fa702-904f-47dd-b365-f67494102055",
-                  "000fa702-904f-47dd-b365-f67494102055"
-                ],
-                "PATIENTIDS": [
-                  "0021E536-383A-48E2-BC76-EA5C7EAC24F9",
-                  "0021E536-383A-48E2-BC76-EA5C7EAC24F9"
-                ],
-                "APPT_LATS": [
-                  37.7749,
-                  34.0522
-                ],
-                "APPT_LNGS": [
-                  -102.4194,
-                  -118.2437
-                ]
-              }
+                "STATE": ["AL", "TX"],
+                "SERVICE_DAY": ["2023-12-05", "2023-12-14"],
+                "APPT_LAT": [31, 30],
+                "APPT_LNG": [-88, -99],
+                "CLIENT": ["Humana", "Aetna_Commercial"],
+                "LOB": ["Medicare", "Commercial"],
+                "GENDERID": [2.0, 1.0],
+                "DATEOFBIRTH": ["1980-12-31", "1960-03-16"],
+                "EMPLOYEETYPENAME": ["1099 Contractor", "1099 Contractor"],
+                "PROVIDERSTATE": ["AL", "TX"],
+                "PROVIDERAGE": [42.0, 63.0],
+                "HIRINGDATE": ["2023-03-13", "2023-05-30"],
+                "TENURE": [267.0, 198.0],
+                "PROD_DSNP": [0.0, 0.0],
+                "PROD_CKD": [0.0, 0.0],
+                "PROD_DEE": [0.0, 0.0],
+                "PROD_FLU": [0.0, 0.0],
+                "PROD_FOBT": [0.0, 0.0],
+                "PROD_SPIROMETRY": [0.0, 0.0],
+                "PROD_HBA1C": [0.0, 0.0],
+                "PROD_HHRA": [1.0, 1.0],
+                "PROD_MHC": [1.0, 0.0],
+                "PROD_MTM": [0.0, 0.0],
+                "PROD_OMW": [0.0, 0.0],
+                "PROD_PAD": [1.0, 0.0],
+                "PROD_VHRA": [0.0, 0.0],
+                "DEGREE": ["NP", "DO"],
+                "VISIT_TIME_MEAN": [29, 42],
+                "VISIT_COUNT": [319, 489],
+            }
         )
     ),
 )
-@output_schema(output_type=StandardPythonParameterType([45.00]))
-
-def run(input_data):
+@output_schema(output_type=StandardPythonParameterType([45.00, 37.7]))
+def run(raw_data):
     """
     This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
     In the example we extract the data from the json input and call the scikit-learn model's predict()
     method and return the result back
     """
-    PROVIDERIDS = input_data["PROVIDERIDS"]
-    PATIENTIDS = input_data["PATIENTIDS"]
-    APPT_LATS = input_data["APPT_LATS"]
-    APPT_LNGS = input_data["APPT_LNGS"]
-    # Convert lists to string format for SQL query
-    provider_ids_str = ','.join(f"'{id}'" for id in PROVIDERIDS)
-    patient_ids_str = ','.join(f"'{id}'" for id in PATIENTIDS)
-    # in future, you can add other fields such as evaluation dy of the week.
-    # Query provider database
 
-    cnt = len(PROVIDERIDS)
-    if cnt != len(PATIENTIDS) or cnt != len(APPT_LATS) or cnt != len(APPT_LNGS):
-        raise ValueError("Length of PROVIDERIDS, PATIENTIDS, APPT_LATS, and APPT_LNGS must be the same")
-    
-    query = f"SELECT \
-                    PROVIDERID, \
-                    PROVIDERSTATE, \
-                    PROVIDERAGE, \
-                    TENURE, \
-                    DEGREE, \
-                    EMPLOYEETYPENAME, \
-                    VISIT_TIME_MEAN, \
-                    VISIT_COUNT \
-               FROM providers WHERE PROVIDERID IN ({provider_ids_str})"
-
-
-    conn = pyodbc.connect(connection_string)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    provider_data = {row[0]: row for row in cursor.fetchall()}
-
-    # Query patient database
-    patient_query = f"SELECT \
-                    PATIENTID, \
-                    STATE, \
-                    CLIENT, \
-                    LOB, \
-                    GENDERID \
-                FROM patients WHERE PATIENTID IN ({patient_ids_str})"
-
-
-    cursor.execute(patient_query)
-    patient_data = {row[0]: row for row in cursor.fetchall()}
-    conn.close()
-
-    # concatenate the values such that the order of columns is:
-    #   PROVIDERSTATE, 
-    #   PROVIDERAGE, 
-    #   TENURE, 
-    #   DEGREE, 
-    #   EMPLOYEETYPENAME, 
-    #   VISIT_TIME_MEAN, 
-    #   VISIT_COUNT, 
-    #   STATE,
-    #   CLIENT,
-    #   LOB,
-    #   GENDERID,
-    #   APPT_LAT, 
-    #   APPT_LNG, 
-    # this order should be the same as the order of columns in the training data
-    input_data = []
-    for i in range(cnt):
-        provider_info = provider_data[PROVIDERIDS[i]][1:]
-        patient_info = patient_data[PATIENTIDS[i]][1:]
-        input_data.append(numpy.concatenate((provider_info, patient_info, [APPT_LATS[i]], [APPT_LNGS[i]])))
-    
+    data = json.loads(raw_data)["input_data"]
 
     column_names = [
-    "PROVIDERSTATE", 
-    "PROVIDERAGE", 
-    "TENURE", 
-    "DEGREE", 
-    "EMPLOYEETYPENAME", 
-    "VISIT_TIME_MEAN", 
-    "VISIT_COUNT", 
-    "STATE",
-    "CLIENT",
-    "LOB",
-    "GENDERID",
-    "APPT_LAT", 
-    "APPT_LNG"
+        "PROVIDERSTATE",
+        "PROVIDERAGE",
+        "TENURE",
+        "DEGREE",
+        "EMPLOYEETYPENAME",
+        "VISIT_TIME_MEAN",
+        "VISIT_COUNT",
+        "STATE",
+        "CLIENT",
+        "LOB",
+        "GENDERID",
+        "APPT_LAT",
+        "APPT_LNG",
     ]
 
     # Convert the input data to a DataFrame
-    input_data = pd.DataFrame(input_data, columns=column_names)
+    input_data = pd.DataFrame(input_data)
+    # filter the columns to the column_names, and order them in the same order in the column_names
+    try:
+        input_data = input_data[column_names]
+    except KeyError as e:
+        logger.error(f"Error: {e}")
+        return [-1] * len(input_data)
 
     # Predict
     results = model.predict(input_data)
